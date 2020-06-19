3d19cbc9c741395a9bd092fa1730405fbf6920e2
chandibhandari1 <49646654+chandibhandari1@users.noreply.github.com>
2020-06-19 16:20:16 -0400

    Add files via upload

--- StreetNumFromAddress.py
+++ StreetNumFromAddress.py
@@ -1,6 +1,7 @@
 """
 Teaching simple use of RegEx to ML Students
 We need the Zipcode and street Address from the given address:
+Note: Here were are only mapping the pattern but we can we other method to get it
 """
 # append the path that for manual foler where install the python packages
 import sys
@@ -40,3 +41,39 @@ regexp2 = "[0-9]{1,5} .+, .+, [A-Z]{2}"
 address2 = re.findall(regexp2,text_given2)
 print(" ")
 print(address2)
+
+# Just extract the Zipcode
+text3 = "3456 notherham blvd Austin, 77643, TX"
+regex3 = '\d{5}'
+reg = re.compile(regex3)
+zipcode1 = reg.findall(text3)
+print("The given zipcode is:",zipcode1)
+#%%%OR%
+reg2 = re.compile('^.*(?P<zipcode>\d{5}).*$')
+zipcode2 = reg2.findall(text3)
+print("The given zipcode by other method:",zipcode2)
+#%%%OR%
+reg3 = re.compile('^.*(?P<zipcode>\d{5}).*$')
+match =reg3.match(text3)
+zipcode3 = match.groupdict()['zipcode']
+print("The given zipcode by other method3:",zipcode3)
+
+
+# Extracting the Street number and street name
+# Street Name, Zipcode and state code
+text3 = "3456 notherham blvd Austin, 77643, TX"
+streetname= re.findall(r'^\d+|\S+ +\S+$', text3)
+print(streetname)
+
+# street number only
+streetnumber =re.split(r'(?<=\d)(?:-\d+)?\s+', text3)[0]
+print(streetnumber)
+# street name and zipcode
+streetname =re.split(r'(?<=\d)(?:-\d+)?\s+', text3)[1]
+print(streetname)
+
+
+
+
+
+
